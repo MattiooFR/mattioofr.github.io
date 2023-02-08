c20a638d2eebaa27e31debc52b34bfb0ddd4bcc3
Mathieu Dugue <dugue.mathieu@gmail.com>
2023-02-08 12:32:44 +0100

    Nikola auto commit.
    Nikola version: 8.2.3

--- conf.py
+++ conf.py
@@ -931,17 +931,38 @@ src="https://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png"></a>"""
 
 MAILCHIMP_SIGNUP = """
 <!-- Begin Mailchimp Signup Form -->
+<link href="//cdn-images.mailchimp.com/embedcode/classic-071822.css" rel="stylesheet" type="text/css">
+<style type="text/css">
+	#mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif;  width:600px;}
+	/* Add your own Mailchimp form style overrides in your site stylesheet or in this style block.
+	   We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+</style>
 <div id="mc_embed_signup">
-<form action="https://dev.us19.list-manage.com/subscribe/post?u=3ffb6593478debd1efe5bf3e7&amp;id=e432d28210" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-    <div id="mc_embed_signup_scroll">
-	<label for="mce-EMAIL">Subscribe</label>
-	<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email" required>
-    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_3ffb6593478debd1efe5bf3e7_e432d28210" tabindex="-1" value=""></div>
-    <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+    <form action="https://brainsorting.us11.list-manage.com/subscribe/post?u=b2649447e3c772d0824ca2f7e&amp;id=4f334f9a34&amp;f_id=0060aae0f0" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+        <div id="mc_embed_signup_scroll">
+        <h2>Subscribe</h2>
+        <div class="indicates-required"><span class="asterisk">*</span> indicates required</div>
+<div class="mc-field-group">
+	<label for="mce-EMAIL">Email Address  <span class="asterisk">*</span>
+</label>
+	<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL" required>
+	<span id="mce-EMAIL-HELPERTEXT" class="helper_text"></span>
+</div>
+	<div id="mce-responses" class="clear foot">
+		<div class="response" id="mce-error-response" style="display:none"></div>
+		<div class="response" id="mce-success-response" style="display:none"></div>
+	</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_b2649447e3c772d0824ca2f7e_4f334f9a34" tabindex="-1" value=""></div>
+        <div class="optionalParent">
+            <div class="clear foot">
+                <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button">
+                <p class="brandingLogo"><a href="http://eepurl.com/ikhw2n" title="Mailchimp - email marketing made easy and fun"><img src="https://eep.io/mc-cdn-images/template_images/branding_logo_text_dark_dtp.svg"></a></p>
+            </div>
+        </div>
     </div>
 </form>
 </div>
+<script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';fnames[3]='ADDRESS';ftypes[3]='address';fnames[4]='PHONE';ftypes[4]='phone';fnames[5]='BIRTHDAY';ftypes[5]='birthday';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
 <!--End mc_embed_signup-->
 """
 
---
+++ posts/creer-un-compte-openai-et-obtenir-sa-cle-api.md
@@ -0,0 +1,41 @@
+<!--
+.. title: Créer un compte OpenAI et obtenir sa clé API
+.. slug: creer-un-compte-openai-et-obtenir-sa-cle-api
+.. date: 2023-02-08 11:51:46 UTC+01:00
+.. tags: ai
+.. category: tutorial
+.. link:
+.. description: quick tutorial to create your openAI account and get your first API key
+.. type: text
+.. previewimage: /images/coding.png
+-->
+
+# Créer un compte OpenAI et obtenir sa clé API
+
+Une clé API est un outil essentiel pour accéder à un serveur, une application ou un service web. Il s'agit d'un code spécifique à votre compte qui est utilisé pour authentifier votre accès et vous fournir des informations et des fonctionnalités spécifiques. Une clé API OpenAI est un code d'accès utilisé pour accéder au moteur GPT-3, un puissant générateur de texte et d'intelligence artificielle. GPT-3 est un système d'intelligence artificielle qui peut être utilisé pour générer et compléter automatiquement des phrases et des paragraphes, et est considéré comme l'un des systèmes les plus avancés à ce jour. La clé API OpenAI est nécessaire pour accéder au moteur GPT-3 et à ses fonctionnalités, et donc pour pouvoir en tirer parti.
+
+<!-- TEASER_END -->
+
+## Créer son compte sur OpenAI
+
+La première étape consiste à se rendre sur le site Web OpenAI et à cliquer sur le lien «S'inscrire» en haut à droite. Vous avez la possibilité de vous inscrire rapidement avec votre compte Google ou Microsoft. Sinon, entrez votre adresse mail, puis choisissez un mot de passe. Une fois que vous avez créé votre compte, vous serez redirigé vers votre page de profil.
+
+![](/images/apiopenai/1.png)
+
+## Obtenir sa clé API
+
+Lorsque vous êtes sur votre page de profil, vous devez à nouveau cliquer en haut à droite sur «Personnel», puis sur «Afficher les clés API».
+
+![](/images/apiopenai/2.png)
+
+Une fois que vous avez cliqué sur ce lien, vous arriverez sur la page où seront affichées vos clés API si vous en possédez déjà une. Par mesure de sécurité, sachez qu’il n’est pas possible d’afficher à nouveau la «clé secrète». Pour créer une nouvelle clé API, cliquez sur «Créer une nouvelle clé secrète».
+
+![](/images/apiopenai/3.png)
+
+En cliquant sur le bouton, une fenêtre s'ouvrira et vous verrez votre clé affichée. Copiez-la bien avant de cliquer sur OK. C'est cette longue chaîne de caractères qu'il faudra enregistrer dans les services que vous souhaitez utiliser qui ont besoin de votre clé API Open AI, ou si vous souhaitez directement coder un programme qui utilise GPT 3.
+
+![](/images/apiopenai/4.png)
+
+Voilà, maintenant vous savez comment créer votre clé API Open AI !
+
+Suivez moi sur Twitter ([@Mattioo81](https://twitter.com/Mattioo81)) plus plus d'infos sur de l'automatisation de l'IA.
